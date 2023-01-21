772b97f1e322c840d4e29eee029d0070a9ab0a42
Marc Fontova <mfontovat@gmail.com>
2023-01-21 11:22:52 +0100

    144

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -7,7 +7,16 @@ class BasicDesignScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [Image(image: AssetImage('assets/landscape.jpg')), Title()],
+        children: [
+          Image(image: AssetImage('assets/landscape.jpg')),
+          Title(),
+          ButtonsSection(),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+            child: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a bibendum ligula. Fusce mattis ex vitae turpis accumsan, sit amet iaculis felis molestie. Ut nec consequat ipsum. Suspendisse eu vulputate magna. Nulla porta nunc lectus, in suscipit augue cursus a. Proin molestie convallis nisl, in vestibulum dui porttitor nec. Sed imperdiet quam a purus elementum, vel mattis nibh consequat.'),
+          ),
+        ],
       ),
     );
   }
@@ -46,6 +55,55 @@ class Title extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class ButtonsSection extends StatelessWidget {
+  const ButtonsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          CustomButton(icon: Icons.phone, text: 'CALL'),
+          CustomButton(icon: Icons.near_me, text: 'ROUTE'),
+          CustomButton(icon: Icons.share, text: 'SHARE'),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          child: Icon(
+            icon,
+            color: Colors.blue,
+          ),
+          margin: EdgeInsets.symmetric(vertical: 5),
+        ),
+        Text(
+          text,
+          style: TextStyle(color: Colors.blue),
+        )
+      ],
     );
   }
 }
