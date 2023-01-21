4aedb85e9d4ae2747468df64ab8dc30a7db09dae
Marc Fontova <mfontovat@gmail.com>
2023-01-21 10:05:09 +0100

    143

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -6,7 +6,46 @@ class BasicDesignScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Image(image: AssetImage('assets/landscape.jpg'))),
+      body: Column(
+        children: [Image(image: AssetImage('assets/landscape.jpg')), Title()],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              Text(
+                'Montaña misteriosa',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text('Lugar inventado')
+            ],
+            crossAxisAlignment: CrossAxisAlignment.start,
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.star,
+                color: Colors.amber[200],
+              ),
+              Text('41')
+            ],
+          )
+        ],
+      ),
     );
   }
 }
