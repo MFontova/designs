f49a45c96d35dcbc05d6ab2f05da66004df95340
Marc Fontova <mfontovat@gmail.com>
2023-01-22 09:31:03 +0100

    150

---
+++ lib/widgets/custom_bottom_navigation.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  const CustomBottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.pink,
+        backgroundColor: Color.fromRGBO(55, 57, 84, 1),
+        unselectedItemColor: Color.fromRGBO(116, 117, 152, 1),
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month), label: 'Calendar'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.pie_chart_outline), label: 'Graphic')
+        ]);
+  }
+}
